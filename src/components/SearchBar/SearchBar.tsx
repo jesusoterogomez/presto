89cc4dff66e43571eaf78525f21302b9fe483077
Jesús Otero <jesusoterogomez@gmail.com>
2021-05-10 14:05:55 +0200

    Fixed SearchBar focusing after item is cleared

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import Select from 'react-select';
+import Select, { components } from 'react-select';
 import { videoSearch, YoutubeSearchResultItem } from '../../api/youtube';
 import { decodeHTMLEntities } from '../../utils/html';
+import FeatherIcon from 'feather-icons-react';
 import './SearchBar.less';
 
-const SearchBar: () => JSX.Element = () => {
+type Props = {
+  onChange: (videoUrl: string) => void;
+};
+
+const SearchBar = (props: Props): JSX.Element => {
+  let ref: any;
+  const [openMenuOnFocusFix, setOpenMenuOnFocusFix] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
   const [videos, setVideos] = useState([] as YoutubeSearchResultItem[]);
 
@@ -21,12 +28,14 @@ const SearchBar: () => JSX.Element = () => {
     };
 
     updateVideos();
-
-    console.log('videos are:', videos);
   }, [searchTerm]);
 
   const handleChange = (updatedTerm: string) => {
     setSearchTerm(updatedTerm);
+  };
+
+  const handleSelectVideo = (videoId: string) => {
+    props.onChange(`https://www.youtube.com/watch?v=${videoId}`);
   };
 
   const formatOptions = (options: YoutubeSearchResultItem[] = []) => {
@@ -40,16 +49,106 @@ const SearchBar: () => JSX.Element = () => {
   const options = formatOptions(videos);
   return (
     <Select
-      components={{ Option }}
+      ref={(node) => (ref = node)}
+      components={{ Option, DropdownIndicator, Control }}
       inputValue={searchTerm}
+      autoFocus={true}
       options={options}
+      openMenuOnFocus={openMenuOnFocusFix}
+      blurInputOnSelect={true}
       onInputChange={handleChange}
       placeholder="Type to search"
       isClearable={true}
+      onChange={(item) => (item ? handleSelectVideo(item.value) : null)}
       backspaceRemovesValue={true}
       openMenuOnClick={false}
       classNamePrefix="searchbar"
+      reset={async () => {
+        const _ref = ref;
+        ref.select.clearValue();
+        ref.select.blur();
+        ref.select.focus();
+
+        /**
+         * To re-focus the input after the search term is cleared, we need
+         * to do some serious black magic. 🧙‍♂️
+         *
+         * react-select has an issue (https://github.com/JedWatson/react-select/issues/3832)
+         * where the caret of the search input doesn't show after re-focusing the input programatically (via `focus()`)
+         *
+         * The github issue above explains that setting openMenuOnFocus to {true} fixes this issue, but this is
+         * not the UX we're looking for. So I came up with a solution that's invisible to the end user.
+         *
+         * In 2 execution cycles (milliseconds), we toggle openMenuOnFocus to true, then false, which
+         * sets focus back into the input element of the react-select control.
+         *
+         * In the 3rd millisecond, we focus the select element again to place the cursor back on the input
+         *
+         * (All of this happens too quick for the end user to see, and is TERRIBLE code, but works like a charm)
+         */
+
+        setTimeout(() => {
+          _ref.select.blur();
+
+          setOpenMenuOnFocusFix(true);
+          _ref.select.focus();
+        }, 1);
+
+        setTimeout(() => {
+          _ref.select.blur();
+          setOpenMenuOnFocusFix(false);
+        }, 2);
+
+        setTimeout(() => {
+          _ref.select.focus();
+        }, 3);
+
+        setVideos([]);
+      }}
     />
+  );
+};
+
+// Good: Custom component declared outside of the Select scope
+const Control = (props: any) => {
+  const { children, ...rest } = props;
+  // console.warn(rest);
+  if (rest.hasValue) {
+    const [value] = rest.getValue();
+    return (
+      <components.Control {...rest} className="ControlVideoSelected">
+        <div className="VideoSelectedInfo">
+          <img src={value.item.snippet.thumbnails.default.url} alt="" />
+          <p>{value.item.snippet.title}</p>
+        </div>
+        <button
+          className="ControlVideoSelectedCancel"
+          onClick={
+            () => rest.selectProps.reset()
+            // console.warn('Do something here', rest)
+          }
+        >
+          Change&nbsp;
+          <FeatherIcon icon="rotate-ccw" size="12" stroke="#FFF" />
+        </button>
+      </components.Control>
+    );
+  }
+
+  return <components.Control {...rest}>{children}</components.Control>;
+};
+
+const DropdownIndicator = (props: any) => {
+  const { ...rest } = props;
+
+  if (rest.hasValue) {
+    return null;
+  }
+
+  return (
+    <components.DropdownIndicator className="DropdownIndicator" {...rest}>
+      <FeatherIcon icon="search" size="24" stroke="#777" />
+    </components.DropdownIndicator>
   );
 };
 
